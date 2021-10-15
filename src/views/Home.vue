f62cd254121884f95e99ce80db0b9dff770eed1b
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-16 00:05:22 +0900

    add status view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <template>
-  <hello-world />
+  <v-content>
+    <v-container>
+      <v-layout wrap>
+        <v-flex md4>
+          <v-card class="mx-4">
+            <v-card-title>Todo</v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex md4>
+          <v-card class="mx-4">
+            <v-card-title>Doing</v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex md4>
+          <v-card class="mx-4">
+            <v-card-title>Done</v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+import Vue from "vue";
 
-  export default Vue.extend({
-    name: 'Home',
+export default Vue.extend({
+  name: "Home",
 
-    components: {
-      HelloWorld,
-    },
-  })
+  components: {},
+});
 </script>

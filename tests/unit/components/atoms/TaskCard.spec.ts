12ace6d6249b63a1273475b47f2cf17a752b7f3b
yuya-okada527 <yuya.okada527@gmail.com>
2021-10-23 21:17:51 +0900

    add task card test

---
+++ tests/unit/components/atoms/TaskCard.spec.ts
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+import TaskCard from "@/components/atoms/TaskCard.vue";
+
+describe("TaskCard", () => {
+  describe("props", () => {
+    it("タスク名が表示されていること", async () => {
+      const wrapper = shallowMount(TaskCard, {
+        propsData: {
+          task: {
+            name: "Test Name",
+            id: 1,
+            status: "todo",
+          },
+        },
+        stubs: ["router-link"],
+      });
+      expect(wrapper.text()).toBe("Test Name");
+    });
+  });
+});

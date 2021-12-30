aa8421bd4d80dcdda61e2b3d9dd531677ae0818b
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-31 00:12:33 +0900

    add task list component

---
+++ components/task-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="task-list-card">
+    <h3 class="task-status">{{ state.status }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    status: String,
+  },
+  async setup(props) {
+    const state = reactive({
+      status: props.status,
+    });
+    return {
+      state,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.task-list-card {
+  width: 180px;
+  height: 80vh;
+  border-radius: 8px;
+  border: solid 1px #ddd;
+  margin: 0px 4px;
+}
+.task-status {
+  color: #444;
+  margin-left: 8px;
+}
+</style>
--- pages/[project_id]/tasks/index.vue
+++ pages/[project_id]/tasks/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <h1>Tasks</h1>
+  <div>
+    <div class="main-header">
+      <h2>Tasks</h2>
+    </div>
+    <div>
+      <ul class="task-board">
+        <li><TaskList status="Created" /></li>
+        <li><TaskList status="Todo" /></li>
+        <li><TaskList status="Doing" /></li>
+        <li><TaskList status="Done" /></li>
+        <li><TaskList status="Closed" /></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+export default defineComponent({
+  async setup() {
+    // TODO implements
+  },
+});
+</script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.task-board {
+  display: flex;
+}
+</style>

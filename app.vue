b90f0ecad9e7ba443b261608f354d75dee2698af
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-30 20:55:35 +0900

    add global css

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <AppHeader />
     <NuxtPage />
   </div>
 </template>
+
+<style scoped lang="scss">
+body {
+  margin: 0;
+}
+</style>
---
+++ components/app-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <h1 class="title">Panopticon</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: blue;
+  color: white;
+}
+.title {
+  margin: 0;
+}
+</style>

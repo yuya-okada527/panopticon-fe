a4861d7ebc57edcabef02d726fed51aae417e4dd
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-31 21:13:00 +0900

    add create task provider

--- pages/[project_id]/task_providers/index.vue
+++ pages/[project_id]/task_providers/index.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="header">
       <h2>Providers</h2>
+      <AddButton @click="onClickAddButton" />
     </div>
     <ul class="provider-cards">
       <li v-for="provider in state.taskProviders">
@@ -13,6 +14,7 @@
 
 <script lang="ts">
 import ApiUrls from "~~/network/static/api-urls";
+import AddButton from "~~/components/add-button.vue";
 
 type State = {
   taskProviders: any;
@@ -21,17 +23,22 @@ type State = {
 export default defineComponent({
   async setup() {
     const route = useRoute();
+    const router = useRouter();
     const { data: taskProviders } = await useFetch(
       ApiUrls.getTaskProvidersUrl(route.params.project_id)
     );
-
     const state: State = {
       taskProviders: taskProviders.value,
     };
+    const onClickAddButton = () => {
+      router.push(`/${route.params.project_id}/new_task_provider`);
+    };
     return {
       state,
+      onClickAddButton,
     };
   },
+  components: { AddButton },
 });
 </script>
 

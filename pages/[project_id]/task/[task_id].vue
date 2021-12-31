9ed061e041856594c9111c560ee34c3ed9facf8a
yuya-okada527 <yuya.okada527@gmail.com>
2021-12-31 14:45:11 +0900

    blush up task detail

--- pages/[project_id]/task/[task_id].vue
+++ pages/[project_id]/task/[task_id].vue
@@ -1,5 +1,25 @@
 <template>
-  <h2>{{ state.task.name }}</h2>
+  <div>
+    <div class="header">
+      <h2>{{ state.task.name }}</h2>
+      <a :href="state.task.external_url" class="external-link">{{
+        "#" + state.task.id
+      }}</a>
+    </div>
+    <div class="status">
+      <label for="status">Status: </label>
+      <select v-model="state.task.status" name="status" id="status">
+        <option value="created">Created</option>
+        <option value="todo">Todo</option>
+        <option value="doing">Doing</option>
+        <option value="done">Done</option>
+        <option value="closed">Closed</option>
+      </select>
+    </div>
+    <textarea name="" id="4" cols="80" rows="30">
+      {{ state.task.description }}
+    </textarea>
+  </div>
 </template>
 
 <script lang="ts">
@@ -25,4 +45,17 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+}
+.external-link {
+  margin-left: 8px;
+  color: blue;
+  text-decoration: none;
+}
+.status {
+  margin: 8px 0;
+}
+</style>
